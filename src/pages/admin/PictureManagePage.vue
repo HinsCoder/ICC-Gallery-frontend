9fee43f8447c9c7109c5494ab6ceaa6d8e702d83
HinsCoder <860845582@qq.com>
2025-02-09 21:13:05 +0800

    Image module - Frontend 1. Image upload and creation/modification page 2. Image management page 3. Image List Page (Homepage) 4. Image details page 5. Image download

--- src/pages/admin/PictureManagePage.vue
+++ src/pages/admin/PictureManagePage.vue
@@ -1,12 +1,30 @@
 <template>
-  <div id="userManagePage">
+  <div id="pictureManagePage">
+    <!-- 标题 -->
+    <a-flex justify="space-between">
+      <h2>图片管理</h2>
+      <a-button type="primary" href="/add_picture" target="_blank">+ 创建图片</a-button>
+    </a-flex>
     <!-- 搜索表单 -->
     <a-form layout="inline" :model="searchParams" @finish="doSearch">
-      <a-form-item label="账号">
-        <a-input v-model:value="searchParams.userAccount" placeholder="输入账号" allow-clear />
+      <a-form-item label="关键词" name="searchText">
+        <a-input
+          v-model:value="searchParams.searchText"
+          placeholder="从名称和简介搜索"
+          allow-clear
+        />
       </a-form-item>
-      <a-form-item label="用户名">
-        <a-input v-model:value="searchParams.userName" placeholder="输入用户名" allow-clear />
+      <a-form-item label="类型" name="category">
+        <a-input v-model:value="searchParams.category" placeholder="请输入类型" allow-clear />
+      </a-form-item>
+      <a-form-item label="标签" name="tags">
+        <a-select
+          v-model:value="searchParams.tags"
+          mode="tags"
+          placeholder="请输入标签"
+          style="min-width: 180px"
+          allow-clear
+        />
       </a-form-item>
       <a-form-item>
         <a-button type="primary" html-type="submit">搜索</a-button>
@@ -19,24 +37,39 @@
       :data-source="dataList"
       :pagination="pagination"
       @change="doTableChange"
+      :scroll="{x:'max-content'}"
     >
       <template #bodyCell="{ column, record }">
-        <template v-if="column.dataIndex === 'userAvatar'">
-          <a-image :src="record.userAvatar" :width="80" />
+        <template v-if="column.dataIndex === 'url'">
+          <a-image :src="record.url" :width="120" />
         </template>
-        <template v-else-if="column.dataIndex === 'userRole'">
-          <div v-if="record.userRole === 'admin'">
-            <a-tag color="green">管理员</a-tag>
-          </div>
-          <div v-else>
-            <a-tag color="blue">普通用户</a-tag>
-          </div>
+        <!-- 标签 -->
+        <template v-if="column.dataIndex === 'tags'">
+          <a-space wrap>
+            <a-tag v-for="tag in JSON.parse(record.tags || '[]')" :key="tag">{{ tag }}</a-tag>
+          </a-space>
+        </template>
+        <!-- 图片信息 -->
+        <template v-if="column.dataIndex === 'picInfo'">
+          <div>格式：{{ record.picFormat }}</div>
+          <div>宽度：{{ record.picWidth }}</div>
+          <div>高度：{{ record.picHeight }}</div>
+          <div>宽高比：{{ record.picScale }}</div>
+          <div>大小：{{ (record.picSize / 1024).toFixed(2) }}KB</div>
         </template>
         <template v-else-if="column.dataIndex === 'createTime'">
           {{ dayjs(record.createTime).format('YYYY-MM-DD HH:mm:ss') }}
         </template>
+        <template v-else-if="column.dataIndex === 'editTime'">
+          {{ dayjs(record.editTime).format('YYYY-MM-DD HH:mm:ss') }}
+        </template>
         <template v-else-if="column.key === 'action'">
-          <a-button danger @click="doDelete(record.id)">删除</a-button>
+          <a-space>
+            <a-button type="link" :href="`/add_picture?id=${record.id}`" target="_blank">
+              编辑
+            </a-button>
+            <a-button type="link" danger @click="doDelete(record.id)">删除</a-button>
+          </a-space>
         </template>
       </template>
     </a-table>
@@ -45,7 +78,10 @@
 <script lang="ts" setup>
 import { SmileOutlined, DownOutlined } from '@ant-design/icons-vue'
 import { computed, onMounted, reactive, ref } from 'vue'
-import { deleteUserUsingPost, listUserVoByPageUsingPost } from '@/api/userController.ts'
+import {
+  deletePictureUsingPost,
+  listPictureByPageUsingPost,
+} from '@/api/pictureController.ts'
 import { message } from 'ant-design-vue'
 import dayjs from 'dayjs'
 
@@ -53,31 +89,46 @@ const columns = [
   {
     title: 'id',
     dataIndex: 'id',
+    width: 80,
   },
   {
-    title: '账号',
-    dataIndex: 'userAccount',
+    title: '图片',
+    dataIndex: 'url',
   },
   {
-    title: '用户名',
-    dataIndex: 'userName',
-  },
-  {
-    title: '头像',
-    dataIndex: 'userAvatar',
+    title: '名称',
+    dataIndex: 'name',
   },
   {
     title: '简介',
-    dataIndex: 'userProfile',
+    dataIndex: 'introduction',
+    ellipsis: true,
   },
   {
-    title: '用户角色',
-    dataIndex: 'userRole',
+    title: '类型',
+    dataIndex: 'category',
+  },
+  {
+    title: '标签',
+    dataIndex: 'tags',
+  },
+  {
+    title: '图片信息',
+    dataIndex: 'picInfo',
+  },
+  {
+    title: '用户 id',
+    dataIndex: 'userId',
+    width: 80,
   },
   {
     title: '创建时间',
     dataIndex: 'createTime',
     sorter: true,
+  },
+  {
+    title: '编辑时间',
+    dataIndex: 'editTime',
   },
   {
     title: '操作',
@@ -86,11 +137,11 @@ const columns = [
 ]
 
 // 定义数据
-const dataList = ref<API.UserVO[]>([])
+const dataList = ref<API.Picture[]>([])
 const total = ref(0)
 
 // 搜索条件
-const searchParams = reactive<API.UserQueryRequest>({
+const searchParams = reactive<API.PictureQueryRequest>({
   current: 1,
   pageSize: 10,
 })
@@ -100,7 +151,7 @@ const sortOrder = ref<'ascend' | 'descend' | null>(null)
 
 // 获取数据
 const fetchData = async () => {
-  const res = await listUserVoByPageUsingPost({
+  const res = await listPictureByPageUsingPost({
     ...searchParams,
     sortField: 'createTime', // 固定排序字段为创建时间
     sortOrder: sortOrder.value ?? undefined, // 使用当前的排序状态
@@ -151,7 +202,7 @@ const doDelete = async (id: string) => {
   if (!id) {
     return
   }
-  const res = await deleteUserUsingPost({ id })
+  const res = await deletePictureUsingPost({ id })
   if (res.data.code === 0) {
     message.success('删除成功')
     // 刷新数据
